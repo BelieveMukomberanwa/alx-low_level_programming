49b9028acdee1bf4039f77225805b7f677d470cf
BelieveMukomberanwa <mukobelieve@gmail.com>
2022-09-15 16:55:10 -0700

    new commits

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,8 +4,9 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-int largest_number(int, int, int);
+int largest_number(int a, int b, int c);
 void print_remaining_days(int, int, int);
 int convert_day(int, int);
+void print_times_table(int n);
 
-#endif /* MAIN_H */
+#endif 
