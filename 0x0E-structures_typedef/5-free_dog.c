d958d8ebe86f99619b13006618e5f8920cb9665f
BelieveMukomberanwa <mukobelieve@gmail.com>
2022-10-13 20:04:41 -0700

    new commits

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,16 +1,17 @@
-include "dog.h"
+#include "dog.h"
 
 #include <stdlib.h>
 
 
 /**
- * free_dog - Frees dogs.
- * @d: The dog to be freed.
- */
+* free_dog - Frees dogs.
+* @d: The dog to be freed.
+*/
 
 void free_dog(dog_t *d)
 
 {
+
 if (d == NULL)
 
 return;
